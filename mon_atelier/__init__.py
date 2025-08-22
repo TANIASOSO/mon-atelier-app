9c466a7a35c01b963a3e34a8d09fc9618a5d8932
TANIA SOSO <innusosophie@yahoo.com>
2025-08-23 00:55:41 +0200

    Corrige le chargement de la configuration

--- mon_atelier/__init__.py
+++ mon_atelier/__init__.py
@@ -15,9 +15,9 @@ app = Flask(__name__, static_folder='static', static_url_path='/static')
 # -- Configuration de la base de données
 database_url = os.environ.get('DATABASE_URL')
 if database_url:
-	app.config['SQLALCHEMY_DATABASE_URI'] = database_url.replace("postgres://", "postgresql://", 1)
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url.replace("postgres://", "postgresql://", 1)
 else:
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'retouches.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'retouches.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['LANG'] = 'fr'
 
@@ -30,25 +30,25 @@ app.config['PAULA_COUTURE_EMAIL'] = "paula.couture@ymail.com"
 app.config['PAULA_COUTURE_SIRET'] = "789 369 584"
 app.config['TVA_RATE'] = 0.20 
 
-# --- INITIALISATION DES EXTENSIONS ---
-db = SQLAlchemy(app)
-twilio_client = Client(app.config['TWILIO_ACCOUNT_SID'], app.config['TWILIO_AUTH_TOKEN'])
-
 # On lit les secrets depuis les variables d'environnement
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['TWILIO_ACCOUNT_SID'] = os.environ.get('TWILIO_ACCOUNT_SID')
 app.config['TWILIO_AUTH_TOKEN'] = os.environ.get('TWILIO_AUTH_TOKEN')
 app.config['TWILIO_PHONE_NUMBER'] = os.environ.get('TWILIO_PHONE_NUMBER')
 
+# --- INITIALISATION DES EXTENSIONS ---
+db = SQLAlchemy(app)
+twilio_client = Client(app.config['TWILIO_ACCOUNT_SID'], app.config['TWILIO_AUTH_TOKEN'])
+
 # --- CONTEXT PROCESSOR (pour rendre 'config' disponible dans tous les templates) ---
 @app.context_processor
 def inject_config():
-	return dict(config=app.config)
+    return dict(config=app.config)
 
 # --- CONTEXT PROCESSOR pour rendre 'timedelta' disponible dans tous les templates ---
 @app.context_processor
 def inject_timedelta():
-	return dict(timedelta=timedelta)
+    return dict(timedelta=timedelta)
 
 # --- IMPORT DES ROUTES ---
 from mon_atelier import routes
