1b894ff8abeed373b6eeefe7a0d9bcdc24aaa2e7
sophie <innusosophie@yahoo.com>
2025-08-24 14:45:08 +0200

    Update run.py to use environment variable for port

--- run.py
+++ run.py
@@ -4,7 +4,9 @@ from mon_atelier import app, db
 # Create database tables if they don't exist
 with app.app_context():
     db.create_all()
+    
+import os
 
-if __name__ == '__main__':
-    # Mode développement (debug + rechargement auto)
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
