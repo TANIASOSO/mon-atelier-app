207d9f29617a7d13bd84e5219a9937f04a9ac7c9
sophie <innusosophie@yahoo.com>
2025-11-01 13:30:25 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,13 @@
 from mon_atelier import app, db
 from mon_atelier.routes import seed_data  
 from dotenv import load_dotenv
+import os
+
 load_dotenv()
 
 with app.app_context():
     db.create_all()
     seed_data()  
-    
-__all__ = ['app']
-
-import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
