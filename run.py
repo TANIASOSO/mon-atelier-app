76e0322c0d4a0f22f6615c8e2ca000747c76012e
TANIASOSO <innusosophie@yahoo.com>
2025-11-10 09:45:05 +0100

    Corrige la configuration de la SECRET_KEY dans run.py

--- run.py
+++ run.py
@@ -1,15 +1,23 @@
 from mon_atelier import app, db
-from mon_atelier.routes import seed_data  
+from mon_atelier.routes import seed_data 
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-with app.app_context():
-    db.create_all()
-    # seed_data()  
+# === C'EST ICI QU'IL FAUT METTRE LA CONFIGURATION ===
+# De cette façon, Gunicorn (sur Railway) et "python run.py" (localement)
+# la verront avant que l'application ne démarre.
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+
+# === NE PAS LANCER db.create_all() ICI ===
+# Ta commande "flask db upgrade" sur Railway s'en occupe.
+# with app.app_context():
+#   db.create_all()
+#   # seed_data() 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    debug_mode = os.environ.get("FLASK_ENV") == "development"
-    app.run(host="0.0.0.0", port=port, debug=debug_mode)
+  port = int(os.environ.get("PORT", 5000))
+  debug_mode = os.environ.get("FLASK_ENV") == "development"
+  app.run(host="0.0.0.0", port=port, debug=debug_mode)
